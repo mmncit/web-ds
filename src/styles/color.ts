56f6f1d1f72060376399c85b4631bf37052649f2
mmncit <mamun.rashid@bentley.com>
2022-08-29 00:05:13 -0600

    add color

---
+++ src/styles/color.ts
@@ -0,0 +1,9 @@
+export const color = {
+    black: '#171321',
+    dkblue: '#0d314b',
+    plum: '#4b0d49',
+    hotmag: '#ff17e4',
+    magenta: '#e310cb',
+    aqua: '#86fbfb',
+    white: '#f7f8fa',
+};
